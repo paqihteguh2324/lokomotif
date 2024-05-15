d959c4ac4112e4dea4fad2051ff6bd401691e67e
Paqih Teguh Maulana <75491223+paqihteguh2324@users.noreply.github.com>
2024-05-15 16:38:10 +0700

    Delete scheduler-info directory

--- scheduler-info/src/main/java/com/lokomotif/schedulerinfo/repository/LokoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lokomotif.schedulerinfo.repository;
-
-import com.lokomotif.schedulerinfo.model.Lokomotif;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LokoRepository extends MongoRepository<Lokomotif, Long> {
-}
